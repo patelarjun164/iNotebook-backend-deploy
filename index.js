b8d6107cf059f01f6b5ad41d2977b6b688d5f1b0
Arjun Patel <92094326+patelarjun164@users.noreply.github.com>
2023-07-25 17:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var cors = require('cors')
 connectToMongo();
 const app = express();
 app.use(cors())
-const port = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-// app.listen(port, ()=> {
-//     console.log("iNotebook App listening at port http://localhost:" + port);
-// })
+app.listen(PORT, ()=> {
+    console.log("iNotebook App listening at port" + PORT);
+})

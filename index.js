dec59dc116bed2b43a7e157cbb03522ad67f0c21
Arjun Patel <92094326+patelarjun164@users.noreply.github.com>
2023-07-25 19:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
-const API_URL = "https://inotebookbackendapi.onrender.com";
-
 //Available Routes
-app.use(`${API_URL}/api/auth`, require('./routes/auth'));
-app.use(`${API_URL}/api/notes`, require('./routes/notes'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(PORT, ()=> {
     console.log("iNotebook App listening at port" + PORT);

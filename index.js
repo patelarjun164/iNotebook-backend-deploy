980c1ed16027fd910fd6ebe545a11f74abda4c5f
Arjun Patel <92094326+patelarjun164@users.noreply.github.com>
2023-07-25 20:01:31 +0530

    Added options for cors

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var cors = require('cors')
 
 connectToMongo();
 const app = express();
-app.use(cors());
+var corsOptions = {
+  origin: 'https://inotebookbackendapi.onrender.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
